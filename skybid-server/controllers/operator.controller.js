2d8e158ef5021618c3f12f46e0a980b44098e674
Fady Dib <fady.dib@hotmail.com>
2023-04-24 10:52:01 +0300

    Add fleet api

---
+++ skybid-server/controllers/operator.controller.js
@@ -0,0 +1,40 @@
+const multer = require('multer');
+const Fleet = require('../models/fleetModel')
+
+const storageEngine = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/') 
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname) 
+    }
+})
+
+const upload = multer({ storage: storageEngine, limits: { fileSize: 10000000000 }, })
+
+exports.addAircraft = async (req,res) => {
+
+    try {
+        const { operator_id, aircraft, passengers, year_of_manufacture } = req.body;
+        
+        const files = req.files;
+        
+        const image = files.map((file) => file.path);
+    
+        const newFleet = new Fleet({
+          operator_id,
+          aircraft,
+          image,
+          passengers,
+          year_of_manufacture
+        });
+        
+        await newFleet.save();
+        
+        res.status(201).json({ message: "Aircraft added successfully!" });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong. Please try again later." });
+      }
+
+}
