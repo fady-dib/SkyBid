389b37d7564789dfdb0b3a49006af3e627158415
Fady Dib <fady.dib@hotmail.com>
2023-04-24 23:00:26 +0300

    Backend: Remove admins from get users

--- skybid-server/controllers/admin.controller.js
+++ skybid-server/controllers/admin.controller.js
@@ -2,7 +2,7 @@ const User = require("../models/userModel");
 const Request = require("../models/requestModel");
 
 exports.getUsers = async (req,res) => {
-    const users = await User.find()
+    const users = await User.find({ role: { $ne: 'admin' } })
     return res.json(users)
 }
 
