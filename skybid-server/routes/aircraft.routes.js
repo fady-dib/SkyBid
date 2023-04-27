580ec4b49ba190268adfdfc26d36dc255400c4aa
Fady Dib <fady.dib@hotmail.com>
2023-04-27 04:03:17 +0300

    Backend: Add get aircrafts by operator route

--- skybid-server/routes/aircraft.routes.js
+++ skybid-server/routes/aircraft.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 
-const {addAircraft, deleteAircraft, updateAircraft} = require('../controllers/aircraft.controller')
+const {addAircraft, deleteAircraft, updateAircraft, getAircraftsByOperator} = require('../controllers/aircraft.controller')
 
 router.post("", addAircraft);
 router.delete("/:id", deleteAircraft);
-router.put("/:id", updateAircraft)
+router.put("/:id", updateAircraft);
+router.get("",getAircraftsByOperator)
 
 
 module.exports = router;
