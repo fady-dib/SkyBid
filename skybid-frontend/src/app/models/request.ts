a5d878302c5e3abe9be15892910a1543f8809427
Fady Dib <fady.dib@hotmail.com>
2023-05-01 02:47:26 +0300

    Frontend: Create request model

---
+++ skybid-frontend/src/app/models/request.ts
@@ -0,0 +1,11 @@
+export class Request {
+    broker : string;
+    trip: string;
+    passengers : number;
+    luggage: number;
+    from: string;
+    to: string;
+    departure_date: Date;
+    return_date: Date;
+    status: string;
+}
