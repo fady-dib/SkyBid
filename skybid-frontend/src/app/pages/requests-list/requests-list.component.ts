7872f8728c280d16de2ca63f77b087ac88779270
Fady Dib <fady.dib@hotmail.com>
2023-05-01 03:45:20 +0300

    Frontend: Add requests list grid

--- skybid-frontend/src/app/pages/requests-list/requests-list.component.ts
+++ skybid-frontend/src/app/pages/requests-list/requests-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GridDataResult } from '@progress/kendo-angular-grid';
+import { SortDescriptor, orderBy } from '@progress/kendo-data-query';
 import { finalize, tap } from 'rxjs';
+import { Request } from 'src/app/models/request';
 import { ApiService } from 'src/app/services/api.service';
 
 @Component({
@@ -19,15 +22,33 @@ constructor(
 
 ) { }
 
-requests = []
+gridView : GridDataResult;
+sort = [] ;
+loading = false;
+
+
+requests: Request[] = []
 
 private search () {
   this.apiService.requests()
   .subscribe(data => {
     this.requests = data
+    this.loadItems()
     console.log(this.requests)
   })
   
+}
+
+public sortChange(sort : SortDescriptor[]) : void{
+  this.sort = sort;
+  this.loadItems()
+}
+
+private loadItems(): void {
+  this.gridView = {
+    data: orderBy(this.requests, this.sort),
+    total: this.requests.length
+  };
 }
 
 }
