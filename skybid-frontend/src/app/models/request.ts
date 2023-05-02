52a778244fbe8ab29103aa64d42033cf2eb8ae3b
Fady Dib <fady.dib@hotmail.com>
2023-05-02 03:36:37 +0300

    Frontend: Fix model.broker bug

--- skybid-frontend/src/app/models/request.ts
+++ skybid-frontend/src/app/models/request.ts
@@ -1,15 +1,24 @@
 export class Request {
-    _id : string;
-    broker : {};
+    _id: string;
+    broker: {
+        company_name:string;
+        role:string;
+        email: string;
+        country: string;
+        city:string
+        address: string;
+        phone:number;
+        bio?:string;
+    };
     trip: string;
-    passengers : number;
+    passengers: number;
     luggage: number;
     from: string;
     to: string;
     departure_date: Date;
     return_date: Date;
     status: string;
-    createdAt : Date;
-    bids : [];
-    updatedAt : Date
+    createdAt: Date;
+    bids: [];
+    updatedAt: Date
 }
