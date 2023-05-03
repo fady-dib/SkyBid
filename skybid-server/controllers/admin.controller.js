c11c32c5981b8cc77a55d08805d9e3ff4314d254
Fady Dib <fady.dib@hotmail.com>
2023-05-03 21:21:57 +0300

    Backend: Remove users from get users api

--- skybid-server/controllers/admin.controller.js
+++ skybid-server/controllers/admin.controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/userModel");
 
 exports.getUsers = async (req,res) => {
-    const users = await User.find({ role: { $ne: 'admin' } })
+    const users = await User.find({ role: { $ne: 'admin' } }).select('-password')
     return res.json(users)
 }
