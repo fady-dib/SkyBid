09e005cb09444cb87cf07b5e0698f403a9610c3f
Fady Dib <fady.dib@hotmail.com>
2023-05-03 22:08:09 +0300

    Frontend: Subscribe to get users

--- skybid-frontend/src/app/pages/admin/dashboard/dashboard.component.ts
+++ skybid-frontend/src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.apiService.getUsers().subscribe(data => {
+this.data = data
+      console.log(data)
+   })
+  }
+
+  constructor(
+    private apiService : ApiService
+  ){}
+
+  data : User = new User
+  seriesData: [] =[]
 
 }
