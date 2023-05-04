229737697ad36a245802c0a44560542780664f0f
Fady Dib <fady.dib@hotmail.com>
2023-05-04 05:45:25 +0300

    Frontend: Initialize variables

--- skybid-frontend/src/app/pages/broker-requests/broker-requests.component.ts
+++ skybid-frontend/src/app/pages/broker-requests/broker-requests.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CellClickEvent, GridDataResult } from '@progress/kendo-angular-grid';
+import { SortDescriptor } from '@progress/kendo-data-query';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-broker-requests',
   templateUrl: './broker-requests.component.html',
   styleUrls: ['./broker-requests.component.css']
 })
-export class BrokerRequestsComponent {
+export class BrokerRequestsComponent implements OnInit {
+
+  ngOnInit(): void {
+    
+  }
+
+  public gridData: GridDataResult;
+private dataSubscription: Subscription;
+sort: SortDescriptor[] = [{
+  field: 'createdAt',
+  dir: 'desc'
+}];
+loading = false;
+mySelection : number[] =[];
+clickedItem: Request
+opened = false
+
+cellClick(event: CellClickEvent) {
+
+}
+
+sortChange(sort :SortDescriptor[]){
+
+}
+
+onDblClick(){
+  
+}
 
 }
