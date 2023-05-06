9ae37ff28e8f587120ea1e49b8e4f1cbe22e3bc5
Fady Dib <fady.dib@hotmail.com>
2023-05-06 21:42:34 +0300

    Frontend: Add sort change function

--- skybid-frontend/src/app/pages/broker-requests/broker-requests.component.ts
+++ skybid-frontend/src/app/pages/broker-requests/broker-requests.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CellClickEvent, GridDataResult } from '@progress/kendo-angular-grid';
-import { SortDescriptor } from '@progress/kendo-data-query';
+import { SortDescriptor, orderBy } from '@progress/kendo-data-query';
 import { Subscription, finalize } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
 import { CreateRequestComponent } from '../create-request/create-request.component';
@@ -11,79 +11,92 @@ import { WindowCloseResult, WindowService } from '@progress/kendo-angular-dialog
   templateUrl: './broker-requests.component.html',
   styleUrls: ['./broker-requests.component.css']
 })
+
+
 export class BrokerRequestsComponent implements OnInit {
+
+  requests = []
+
+
   ngOnInit(): void {
     this.search()
   }
 
   constructor(
-    private apiService : ApiService,
-    private windowService : WindowService,
+    private apiService: ApiService,
+    private windowService: WindowService,
   ) { }
-  
+
 
   public gridData: GridDataResult;
-private dataSubscription: Subscription;
-sort: SortDescriptor[] = [{
-  field: 'createdAt',
-  dir: 'desc'
-}];
-loading = false;
-mySelection : number[] =[];
-clickedItem: Request
-opened = false
+  private dataSubscription: Subscription;
+  
+  sort: SortDescriptor[] = [{
+    field: 'createdAt',
+    dir: 'desc'
+  }];
 
-private search() {
-  this.loading = true
-  this.apiService.getRequestsByBroker()
-  .pipe(
-    finalize(()=> this.loading = false)
-  )
-  .subscribe(data => {
-    console.log(data)
+  loading = false;
+  mySelection: number[] = [];
+  clickedItem: Request
+  opened = false
+
+  private search() {
+    this.loading = true
+    this.apiService.getRequestsByBroker()
+      .pipe(
+        finalize(() => this.loading = false)
+      )
+      .subscribe(data => {
+        console.log(data)
+        this.requests = data
+        this.loadItems()
+      })
+  }
+
+  cellClick(event: CellClickEvent) {
+    this.mySelection = event.dataItem._id
+
+    this.clickedItem = event.dataItem;
+    setTimeout(() => {
+      this.clickedItem = null
+    }, 300)
+  }
+
+  public sortChange(sort: SortDescriptor[]): void {
+    this.sort = sort;
+    this.loadItems()
+  }
+
+  private loadItems(): void {
     this.gridData = {
-      data: data, total : data.length
-    }
-  })
-}
+      data: orderBy(this.requests, this.sort),
+      total: this.requests.length
+    };
+  }
 
-cellClick(event: CellClickEvent) {
-  this.mySelection = event.dataItem._id
+  onDblClick() {
 
-  this.clickedItem = event.dataItem;
-  setTimeout(() => {
-    this.clickedItem = null
-  }, 300)
-}
+  }
 
-sortChange(sort :SortDescriptor[]){
 
-}
+  onDelete() {
 
-onDblClick(){
+  }
 
-}
-onEdit() {
-
-}
-
-onDelete(){
-
-}
-
-onAdd(){
-  this.opened = true
-const windowRef = this.windowService.open({
-    title : `CREATE REQUEST`,
-    content: CreateRequestComponent,
-    width :635,
-    top : 100
-  });
-  windowRef.result.subscribe((result) => {
-    if(result instanceof WindowCloseResult) {
-      this.opened =false;
-    }
-  })
-}
+  onAdd() {
+    this.opened = true
+    const windowRef = this.windowService.open({
+      title: `CREATE REQUEST`,
+      content: CreateRequestComponent,
+      width: 635,
+      top: 100
+    });
+    windowRef.result.subscribe((result) => {
+      if (result instanceof WindowCloseResult) {
+        this.opened = false;
+      }
+    })
+  }
 
 }
