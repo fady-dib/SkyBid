121869f287644cfda199db6a04d5edc2ec0d2dd9
Fady Dib <fady.dib@hotmail.com>
2023-05-09 06:21:42 +0300

    Frontend: Add notification when the profile updated successfully

--- skybid-frontend/src/app/pages/edit-profile/edit-profile.component.ts
+++ skybid-frontend/src/app/pages/edit-profile/edit-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService, Position } from '@progress/kendo-angular-notification';
 import { User } from 'src/app/models/user';
 import { ApiService } from 'src/app/services/api.service';
 
@@ -10,6 +11,10 @@ import { ApiService } from 'src/app/services/api.service';
 export class EditProfileComponent implements OnInit {
 
   ngOnInit(): void {
+ this.getUser()
+  }
+
+  getUser(){
     this.apiService.getUser().subscribe(data => {
       this.model = data
       console.log(data)
@@ -17,13 +22,22 @@ export class EditProfileComponent implements OnInit {
   }
 
   constructor(
-    private apiService : ApiService
+    private apiService : ApiService,
+    private notificationService : NotificationService
   ){}
 
   model : User = new User;
+  notifPos : Position = { vertical: 'top', horizontal:'center'}
 
   update(){
-
+this.apiService.updateProfile(this.model).subscribe(() => {
+  this.notificationService.show({
+    content: 'Your profile has been updated successfully',
+    type: { style:'success' },
+    position: this.notifPos,
+  });
+  this.getUser()
+})
   }
 
 }
